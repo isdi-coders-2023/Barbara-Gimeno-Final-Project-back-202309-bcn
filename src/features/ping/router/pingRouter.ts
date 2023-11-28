055714ce6ebdfa9ace7f0c8316f3d1e32f0156da
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-28 18:47:25 +0100

    Add pingRouter

---
+++ src/features/ping/router/pingRouter.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { PingController } from "../controller/pingController.js";
+
+const pingRouter = Router();
+const pingController = new PingController();
+
+pingRouter.get("/", pingController.getPong);
+
+export default pingRouter;
