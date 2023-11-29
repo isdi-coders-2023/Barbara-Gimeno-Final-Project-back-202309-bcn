4dd2716f08469a226f2b257033380a5aaf08ea61
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-29 16:23:38 +0100

    Add CustomError

---
+++ src/CustomError/CustomError.ts
@@ -0,0 +1,11 @@
+export class CustomError extends Error {
+  constructor(
+    message: string,
+    public readonly statusCode: number,
+    public readonly privateMessage?: string,
+  ) {
+    super(message);
+  }
+}
+
+export default CustomError;
