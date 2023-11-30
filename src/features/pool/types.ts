010928a246ffa1734087ae1de4b3efebd11a122c
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-30 11:03:20 +0100

    Add pool types

---
+++ src/features/pool/types.ts
@@ -0,0 +1,14 @@
+export interface PoolDataStructure {
+  title: string;
+  measuresLong: number;
+  measuresHigh: number;
+  measuresWide: number;
+  since: number;
+  depuration: string;
+  material: string;
+  image: string;
+}
+
+export interface PoolStructure extends PoolDataStructure {
+  _id: string;
+}
