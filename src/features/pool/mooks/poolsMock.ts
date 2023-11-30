d434c28e3926700f16e5faf9c63e9907c2dc0eb3
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-30 13:06:39 +0100

    Add poolsMock

---
+++ src/features/pool/mooks/poolsMock.ts
@@ -0,0 +1,26 @@
+import type { PoolDataStructure } from "../types.js";
+
+const poolsMock: PoolDataStructure[] = [
+  {
+    title: "beach pool",
+    measuresLong: 15,
+    measuresHigh: 10,
+    measuresWide: 1,
+    since: 2018,
+    depuration: "electrolisis salty",
+    material: "epoxy",
+    image: "https://i.ibb.co/jv9YYD3/1.webp",
+  },
+  {
+    title: "infinity pool",
+    measuresLong: 25,
+    measuresHigh: 12,
+    measuresWide: 2,
+    since: 2015,
+    depuration: "electrolisis salty",
+    material: "mortal",
+    image: "https://i.ibb.co/7bhAvz6/2.webp",
+  },
+];
+
+export default poolsMock;
