7720845fd4a5cb83daa43ab42d58ff781a6dbc89
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-07 16:30:20 +0100

    Add delete method to PoolsController

--- src/features/pool/controller/PoolsController.ts
+++ src/features/pool/controller/PoolsController.ts
@@ -1,5 +1,6 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { type PoolsRepositoryStructure } from "../repository/typesRepository.js";
+import CustomError from "../../../CustomError/CustomError.js";
 
 class PoolsController {
   constructor(private readonly poolsRepository: PoolsRepositoryStructure) {}
@@ -8,6 +9,21 @@ class PoolsController {
     const pools = await this.poolsRepository.getPools();
 
     res.status(200).json({ pools });
+  };
+
+  deletePool = async (
+    req: Request<{ poolId: string }>,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    const { poolId } = req.params;
+    try {
+      await this.poolsRepository.deletePool(poolId);
+      res.status(200).json({});
+    } catch {
+      const error = new CustomError("Pool can not be deleted", 400);
+      next(error);
+    }
   };
 }
 
