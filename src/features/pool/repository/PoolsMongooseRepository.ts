8b9b2dc4854f0b0f1efe67d4ac88780dfbfe63fe
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-12-11 13:41:02 +0100

    Add method to repository

--- src/features/pool/repository/PoolsMongooseRepository.ts
+++ src/features/pool/repository/PoolsMongooseRepository.ts
@@ -1,12 +1,12 @@
 import Pool from "../model/Pool.js";
-import { type PoolStructure } from "../types.js";
+import { type PoolDataStructure, type PoolStructure } from "../types.js";
 import { type PoolsRepositoryStructure } from "./typesRepository.js";
 
 class PoolsMongooseRepository implements PoolsRepositoryStructure {
   public getPools = async (): Promise<PoolStructure[]> => {
     const limit = 10;
 
-    const pools = await Pool.find().limit(limit);
+    const pools = await Pool.find().limit(limit).sort({ _id: -1 });
 
     return pools;
   };
@@ -16,6 +16,16 @@ class PoolsMongooseRepository implements PoolsRepositoryStructure {
       await Pool.findByIdAndDelete(poolId);
     } catch (error) {
       throw new Error("Error deleting the pool" + (error as Error).message);
+    }
+  }
+
+  async addPool(pool: PoolDataStructure): Promise<PoolStructure> {
+    try {
+      const newPool = await Pool.create(pool);
+
+      return newPool;
+    } catch (error) {
+      throw new Error("Error creating a new pool" + (error as Error).message);
     }
   }
 }
